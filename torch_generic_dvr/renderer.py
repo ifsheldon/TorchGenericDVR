5a5c615da67605392d30527debc63486fbb37d55
ifsheldon <39153080+ifsheldon@users.noreply.github.com>
2021-08-13 22:28:58 +0300

    refactor

--- torch_generic_dvr/renderer.py
+++ torch_generic_dvr/renderer.py
@@ -81,73 +81,6 @@ class RandomCameraPoses(Dataset):
         return torch.tensor(r_mat).float()
 
 
-def calc_volume_weights(alpha):
-    # alpha: shape (batch, res*res, step_num)
-    weights = alpha * \
-              torch.cumprod(torch.cat([
-                  torch.ones_like(alpha[:, :, :1]),
-                  (1. - alpha + 1e-10), ], dim=-1), dim=-1)[..., :-1]
-    return weights
-
-
-def get_evaluation_points(pixels_world, camera_world, di):
-    batch_size = pixels_world.shape[0]
-    ray = pixels_world - camera_world
-    p = camera_world.unsqueeze(-2).contiguous() + \
-        di.unsqueeze(-1).contiguous() * ray.unsqueeze(-2).contiguous()
-    p = p.reshape(batch_size, -1, 3)
-    return p
-
-
-def add_noise_to_interval(di):
-    di_mid = .5 * (di[..., 1:] + di[..., :-1])
-    di_high = torch.cat([di_mid, di[..., -1:]], dim=-1)
-    di_low = torch.cat([di[..., :1], di_mid], dim=-1)
-    noise = torch.rand_like(di_low)
-    ti = di_low + (di_high - di_low) * noise
-    return ti
-
-
-def get_camera_mat(fov, invert=True):
-    focal = 1. / np.tan(0.5 * np.radians(fov))
-    focal = focal.astype(np.float32)
-    mat = torch.tensor([
-        [focal, 0., 0., 0.],
-        [0., focal, 0., 0.],
-        [0., 0., 1, 0.],
-        [0., 0., 0., 1.]
-    ]).reshape(1, 4, 4)
-    if invert:
-        mat = torch.inverse(mat)
-    return mat
-
-
-def transform_to_world(pixels, depth, camera_mat, world_mat, use_absolute_depth=True):
-    ''' Transforms pixel positions p with given depth value d to world coordinates.
-
-    Args:
-        pixels (tensor): pixel tensor of size B x N x 2
-        depth (tensor): depth tensor of size B x N x 1
-        camera_mat (tensor): camera matrix
-        world_mat (tensor): world matrix
-    '''
-    assert (pixels.shape[-1] == 2)
-    # Transform pixels to homogen coordinates
-    pixels = pixels.permute(0, 2, 1)
-    pixels = torch.cat([pixels, torch.ones_like(pixels)], dim=1)
-    # Project pixels into camera space
-    if use_absolute_depth:
-        pixels[:, :2] = pixels[:, :2] * depth.permute(0, 2, 1).abs()
-        pixels[:, 2:3] = pixels[:, 2:3] * depth.permute(0, 2, 1)
-    else:
-        pixels[:, :3] = pixels[:, :3] * depth.permute(0, 2, 1)
-    # Transform pixels to world space
-    p_world = world_mat @ camera_mat @ pixels
-    # Transform p_world back to 3D coordinates
-    p_world = p_world[:, :3].permute(0, 2, 1)
-    return p_world
-
-
 class DirectVolumeRenderer(pl.LightningModule):
     """ Direct Volume Renderer.
 
@@ -172,12 +105,12 @@ class DirectVolumeRenderer(pl.LightningModule):
         self.n_ray_samples = n_ray_samples
         self.feature_img_resolution = feature_img_resolution
         self.depth_range = depth_range
-        self.camera_matrix = nn.Parameter(get_camera_mat(fov))
+        self.camera_matrix = nn.Parameter(self.get_camera_mat(fov))
         self.volume_sampler = TrilinearVolumeSampler(volume)
         self.tf = TransferFunctionModel1D(transfer_function_data)
         self.neural_renderer = None if neural_renderer is None else neural_renderer
 
-    def arrange_pixels(self, resolution=(128, 128), batch_size=1, image_range=(-1., 1.), invert_y_axis=False):
+    def arrange_pixels(self, resolution, batch_size, image_range=(-1., 1.)):
         ''' Arranges pixels for given resolution in range image_range.
 
         The function returns the unscaled pixel locations as integers and the
@@ -201,11 +134,6 @@ class DirectVolumeRenderer(pl.LightningModule):
         loc = scale / 2
         pixel_scaled[:, :, 0] = scale * pixel_scaled[:, :, 0] / (w - 1) - loc
         pixel_scaled[:, :, 1] = scale * pixel_scaled[:, :, 1] / (h - 1) - loc
-
-        if invert_y_axis:
-            assert (image_range == (-1, 1))
-            pixel_scaled[..., -1] *= -1.
-            pixel_locations[..., -1] = (h - 1) - pixel_locations[..., -1]
 
         return pixel_locations, pixel_scaled
 
@@ -246,7 +174,7 @@ class DirectVolumeRenderer(pl.LightningModule):
         depth_image = torch.ones(batch_size, n_pts, 1).to(self.device)
         if negative_depth:
             depth_image *= -1.
-        return transform_to_world(image_points, depth_image, camera_mat, world_mat)
+        return self.transform_to_world(image_points, depth_image, camera_mat, world_mat)
 
     def forward(self, world_mat, mode="training"):
         batch_size = world_mat.shape[0]
@@ -261,7 +189,7 @@ class DirectVolumeRenderer(pl.LightningModule):
     def cast_ray_and_get_eval_points(self, img_res, batch_size, depth_range, n_steps, camera_mat, world_mat,
                                      ray_jittering):
         # Arrange Pixels
-        pixels = self.arrange_pixels((img_res, img_res), batch_size, invert_y_axis=False)[1]
+        pixels = self.arrange_pixels((img_res, img_res), batch_size)[1]
         pixels[..., -1] *= -1.
         n_points = img_res * img_res
         # Project to 3D world
@@ -273,9 +201,9 @@ class DirectVolumeRenderer(pl.LightningModule):
                               depth_range[1] - depth_range[0])
         step_depths = step_depths.repeat(batch_size, n_points, 1).to(self.device)
         if ray_jittering:
-            step_depths = add_noise_to_interval(step_depths)
-        point_pos_wc = get_evaluation_points(pixel_pos_wc, camera_pos_wc,
-                                             step_depths)  # shape (batch, num of eval points, 3)
+            step_depths = self.add_noise_to_interval(step_depths)
+        point_pos_wc = self.get_evaluation_points(pixel_pos_wc, camera_pos_wc,
+                                                  step_depths)  # shape (batch, num of eval points, 3)
         return point_pos_wc
 
     def volume_render_image(self, camera_mat, world_mat, batch_size, mode):
@@ -302,12 +230,79 @@ class DirectVolumeRenderer(pl.LightningModule):
         features = feature_with_alpha[:, :, :, :-1]
         alphas = feature_with_alpha[:, :, :, -1]
         # DVR composition
-        weights = calc_volume_weights(alphas)
+        weights = self.calc_volume_weights(alphas)
         feat_map = torch.sum(weights.unsqueeze(-1) * features, dim=-2)
         # Reformat output
         feat_map = feat_map.permute(0, 2, 1).reshape(batch_size, -1, img_res, img_res)  # B x feat x h x w
         feat_map = feat_map.permute(0, 1, 3, 2)  # new to flip x/y
         return feat_map
+
+    @staticmethod
+    def transform_to_world(pixels, depth, camera_mat, world_mat, use_absolute_depth=True):
+        ''' Transforms pixel positions p with given depth value d to world coordinates.
+
+        Args:
+            pixels (tensor): pixel tensor of size B x N x 2
+            depth (tensor): depth tensor of size B x N x 1
+            camera_mat (tensor): camera matrix
+            world_mat (tensor): world matrix
+        '''
+        assert (pixels.shape[-1] == 2)
+        # Transform pixels to homogen coordinates
+        pixels = pixels.permute(0, 2, 1)
+        pixels = torch.cat([pixels, torch.ones_like(pixels)], dim=1)
+        # Project pixels into camera space
+        if use_absolute_depth:
+            pixels[:, :2] = pixels[:, :2] * depth.permute(0, 2, 1).abs()
+            pixels[:, 2:3] = pixels[:, 2:3] * depth.permute(0, 2, 1)
+        else:
+            pixels[:, :3] = pixels[:, :3] * depth.permute(0, 2, 1)
+        # Transform pixels to world space
+        p_world = world_mat @ camera_mat @ pixels
+        # Transform p_world back to 3D coordinates
+        p_world = p_world[:, :3].permute(0, 2, 1)
+        return p_world
+
+    @staticmethod
+    def calc_volume_weights(alpha):
+        # alpha: shape (batch, res*res, step_num)
+        weights = alpha * \
+                  torch.cumprod(torch.cat([
+                      torch.ones_like(alpha[:, :, :1]),
+                      (1. - alpha + 1e-10), ], dim=-1), dim=-1)[..., :-1]
+        return weights
+
+    @staticmethod
+    def get_evaluation_points(pixels_world, camera_world, di):
+        batch_size = pixels_world.shape[0]
+        ray = pixels_world - camera_world
+        p = camera_world.unsqueeze(-2).contiguous() + \
+            di.unsqueeze(-1).contiguous() * ray.unsqueeze(-2).contiguous()
+        p = p.reshape(batch_size, -1, 3)
+        return p
+
+    @staticmethod
+    def add_noise_to_interval(di):
+        di_mid = .5 * (di[..., 1:] + di[..., :-1])
+        di_high = torch.cat([di_mid, di[..., -1:]], dim=-1)
+        di_low = torch.cat([di[..., :1], di_mid], dim=-1)
+        noise = torch.rand_like(di_low)
+        ti = di_low + (di_high - di_low) * noise
+        return ti
+
+    @staticmethod
+    def get_camera_mat(fov, invert=True):
+        focal = 1. / np.tan(0.5 * np.radians(fov))
+        focal = focal.astype(np.float32)
+        mat = torch.tensor([
+            [focal, 0., 0., 0.],
+            [0., focal, 0., 0.],
+            [0., 0., 1, 0.],
+            [0., 0., 0., 1.]
+        ]).reshape(1, 4, 4)
+        if invert:
+            mat = torch.inverse(mat)
+        return mat
 
 
 if __name__ == "__main__":
